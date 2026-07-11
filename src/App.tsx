f8290dba4b4b4434ac54531ab072a6705c2e4c47
Scarpato <30941595+faelscarpato@users.noreply.github.com>
2026-07-11 00:04:03 -0300

    Refactor App component to use new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,32 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { Solutions } from './components/Solutions';
+import { Modelos } from './components/Modelos';
 import { HowItWorks } from './components/HowItWorks';
 import { Features } from './components/Features';
+import { Testimonials } from './components/Testimonials';
+import { Guarantee } from './components/Guarantee';
 import { Pricing } from './components/Pricing';
 import { FAQ } from './components/FAQ';
+import { FinalCTA } from './components/FinalCTA';
 import { Footer } from './components/Footer';
 import { WhatsAppButton } from './components/WhatsAppButton';
-import { ScrollReveal } from './components/ScrollReveal';
 
 export default function App() {
   return (
-    <div className="bg-background text-on-background font-body-md antialiased min-h-screen p-4 sm:p-10">
-      <div className="max-w-[1440px] mx-auto border border-outline relative flex flex-col bg-surface-dim min-h-[calc(100vh-80px)] overflow-clip">
-        <Navbar />
-        <ScrollReveal>
-          <Hero />
-        </ScrollReveal>
-        <ScrollReveal>
-          <Solutions />
-        </ScrollReveal>
-        <ScrollReveal>
-          <HowItWorks />
-        </ScrollReveal>
-        <ScrollReveal>
-          <Features />
-        </ScrollReveal>
-        <ScrollReveal>
-          <Pricing />
-        </ScrollReveal>
-        <ScrollReveal>
-          <FAQ />
-        </ScrollReveal>
-        <Footer />
-        <WhatsAppButton />
-      </div>
+    <div className="bg-white text-on-background font-body antialiased">
+      <Navbar />
+      <Hero />
+      <Modelos />
+      <HowItWorks />
+      <Features />
+      <Testimonials />
+      <Guarantee />
+      <Pricing />
+      <FAQ />
+      <FinalCTA />
+      <Footer />
+      <WhatsAppButton />
     </div>
   );
 }
